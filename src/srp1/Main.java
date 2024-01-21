acb9a6d32bc0e47f4df8a8dcce3f5af92eba7027
Dyadyk Alexander <a.dyadyk@gmail.com>
2024-01-21 19:04:07 +0300

    srp1 - complete

--- src/srp1/Main.java
+++ src/srp1/Main.java
@@ -1,12 +1,18 @@
 package srp1;
 
+import srp1.figure.SquarePainter;
 import srp1.figure.Point;
 import srp1.figure.Square;
 
 public class Main {
     public static void main(String[] args) {
+        // Создадим экземпляр класса Square:
         Square square = new Square(new Point(1,1), 5);
+        // Высчитаем площадь созданного экземпляра:
         System.out.printf("Square area: %d \n", square.getArea());
-        square.draw();
+        // Создадим экземпляр класса Painter:
+        SquarePainter painter = new SquarePainter();
+        // Вызовем метод draw():
+        painter.draw(square);
     }
 }

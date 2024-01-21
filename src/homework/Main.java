8d096cfebd75d07ce61ac6e9a2cd9b78f05d8b96
Dyadyk Alexander <a.dyadyk@gmail.com>
2024-01-22 01:14:33 +0300

    homework - complete update

--- src/homework/Main.java
+++ src/homework/Main.java
@@ -10,6 +10,7 @@ public class Main{
         reporter.report(user);
         persister.save(user);
 
+        // Вторая проверка:
         System.out.println();
         User tom = new User("Tom");
         reporter.report(tom);
@@ -22,4 +23,6 @@ public class Main{
 * был перенесен метод report(). Метод save() был удален из класса User так как он реализован в классе Persister.
 * Так же в класс Persister был закрыт для расширения. То есть нарушен "Принцип открытости-закрытости"
 * Было исправлено имплементацией интерфейса Saveable.
+* Также в классе Persister было убрано поле User, теперь user непосредственно передаётся в метод. Аналогично
+* всё выполнено в классе Reporter.
 * */

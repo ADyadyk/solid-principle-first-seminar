6d20ecc837361e0c786920ff540fb8acbf5b45ae
Dyadyk Alexander <a.dyadyk@gmail.com>
2024-01-21 21:59:07 +0300

    isp1 - complete

--- src/isp1/Main.java
+++ src/isp1/Main.java
@@ -3,16 +3,22 @@ package isp1;
 import isp1.pay.impl.InternetPaymentService;
 import isp1.pay.impl.TerminalPaymentService;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         InternetPaymentService internetService = new InternetPaymentService();
         internetService.payWebMoney(10);
-        internetService.payWebMoney(10);
-        internetService.payWebMoney(10);
+        internetService.payCreditCard(10);
+        internetService.payPhoneNumber(10);
 
         TerminalPaymentService terminalService = new TerminalPaymentService();
         terminalService.payWebMoney(10);
         terminalService.payCreditCard(10);
-        terminalService.payPhoneNumber(10);
     }
 }
+/*
+* Был нарушен "Принцип разделения интерфейсов. Интерфейс Payable был разбит на несколько интерфейсов.
+* Каждый конкретный класс, характеризующий форму оплаты, имплементирует только тот интерфейс, который он
+* поддерживает.
+* */

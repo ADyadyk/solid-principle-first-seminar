edd02e9286423a85059eb732b127ba90a90e0542
Dyadyk Alexander <a.dyadyk@gmail.com>
2024-01-21 21:34:00 +0300

    lsp1 - complete

--- src/lsp1/Main.java
+++ src/lsp1/Main.java
@@ -1,14 +1,14 @@
 package lsp1;
 
 import lsp1.shape.Rectangle;
+import lsp1.shape.Shape;
 import lsp1.shape.Square;
 import lsp1.view.ShapeView;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Square(5);
-        rectangle.setSideA(4);
-        rectangle.setSideB(5);
+        Shape shape = new Rectangle(5, 4);
+        Rectangle rectangle = (Rectangle)shape;
         System.out.printf("In a rectangle side A = %d, side B = %d\n", rectangle.getSideA(), rectangle.getSideB());
         ShapeView view = new ShapeView(rectangle);
         view.showArea();

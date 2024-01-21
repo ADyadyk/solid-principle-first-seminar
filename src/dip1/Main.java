f854307be5731175f30b824ad642c2243fba55f8
Dyadyk Alexander <a.dyadyk@gmail.com>
2024-01-21 22:43:46 +0300

    dip1 - complete

--- src/dip1/Main.java
+++ src/dip1/Main.java
@@ -1,6 +1,9 @@
 package dip1;
 
 import dip1.model.Report;
+import dip1.model.ReportManager;
+import dip1.model.util.ConsoleReporter;
+import dip1.model.util.PrinterReporter;
 import logger.Log;
 
 import java.util.logging.Level;
@@ -12,6 +15,18 @@ public class Main {
         log.log(Level.INFO, "Method main in model package started");
         Report report = new Report();
         report.calculate();
-        report.output();
+
+        ReportManager reportManager = new ReportManager(new ConsoleReporter());
+        reportManager.output(report.getItems());
+
+//        ReportManager reportManager = new ReportManager(new PrinterReporter());
+//        reportManager.output(report.getItems());
+
     }
 }
+/*
+* Был нарушен "Принцип инверсии зависимостей". Были добавлены класс ReportManager и интерфейс Reportable.
+* Интерфейс Reportable был имплементирован в PrintReporter и ConsoleReporter. В ReportManager метод output()
+* вызывается через абстракцию, которая реализует метод в зависимости через экземпляр какого класса был создан
+* ReportManager.
+* */
